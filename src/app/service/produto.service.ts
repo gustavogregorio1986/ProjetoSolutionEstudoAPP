404b6a3a8d78d65ca212cdc536707ad5878cb749
gustavo gregorio <gustavogregorio1986@gmail.com>
2024-10-04 16:07:18 -0300

    desenvolver o service no angular do frontend

--- src/app/service/produto.service.ts
+++ src/app/service/produto.service.ts
@@ -1,5 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Produto } from '../classes/produto';
 
 @Injectable({
   providedIn: 'root'
@@ -7,4 +9,22 @@ import { Injectable } from '@angular/core';
 export class ProdutoService {
 
   constructor(private http: HttpClient) { }
+
+  private apiUrl = 'https://localhost:7052/api/Produto'
+
+  adicionarProduto(produto: Produto): Observable<Produto> {
+    return this.http.post<Produto>(`${this.apiUrl}/AdicionarProduto`, produto);
+  }
+  
+  listarProdutos(): Observable<Produto[]> {
+    return this.http.get<Produto[]>(`${this.apiUrl}/ListarProdutos`);
+  }
+
+  listarAtivos(): Observable<Produto[]> {
+    return this.http.get<Produto[]>(`${this.apiUrl}/ListarAtivos`);
+  }
+
+  listarInativos(): Observable<Produto[]> {
+    return this.http.get<Produto[]>(`${this.apiUrl}/ListarInativos`);
+  }
 }
